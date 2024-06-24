39e752fbbf15b6ec1b2e04230cb665ec51daa8dd
Quy Nguyen <quy.nguyenba58@gmail.com>
2024-06-24 07:40:24 +0700

    Improve Code

--- src/routes/servers/servers.controller.ts
+++ src/routes/servers/servers.controller.ts
@@ -4,7 +4,12 @@ import {ServerService} from '@services/server/server.service';
 import {SERVERS} from '@data/servers';
 
 export const findServers = async (_: Request, res: Response) => {
-  const servers = await ServerService.findServer(SERVERS);
-
-  return res.status(200).json(servers);
+  try {
+    const servers = await ServerService.findServer(SERVERS);
+    return res.status(200).json(servers);
+  } catch (error) {
+    return res.status(400).json({
+      message: error?.message || 'Something went wrong',
+    });
+  }
 };
